ac18bdb88cf7cf290f38690c99e0459d2e85d89b
jmjsantos <82263366+jmjsantos@users.noreply.github.com>
2021-10-16 00:53:26 +0800

    Update and rename SANTOS_CacheMatrix.R to cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,26 +1,26 @@
-makeCacheMatrix <- function(a = matrix()) 
+makeCacheMatrix <- function(u = matrix()) 
 {
-  Jm<-NULL
+  Inver<-NULL
   set<-function(g) {
-    a<<-g
+    u<<-g
     inver<<-NULL
 }
-  get<-function() a
-  setInver<-function(inverse) Inver<<-inverse
+  get<-function() u
+  setInver<-function(inverse) {Inver<<-inverse}
   getInver<-function() Inver
   list(set=set,
        get=get,
        setInver=setInver,
        getInver=getInver)
 }
-cacheSolve <- function(a, ...) {
-  inver<-x$getInver()
+cacheSolve <- function(u, ...) {
+  inver<-u$getInver()
   if(!is.null(inver)) {
     message("Matrix being retrieved")
     return(inver)
 }
-  answer<-a$get()
+  answer<-u$get()
   inver<-solve(answer,...)
-  a$setInver(inver)
+  u$setInver(inver)
   inver
 }
